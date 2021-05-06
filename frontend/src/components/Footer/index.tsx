7d51d449ffefd12025524aa36e8f10f6679f7e9d
RonyGomesK78 <gromilton7@gmail.com>
2021-05-05 23:15:16 -0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Powered by <a href="https://www.linkedin.com/in/romiltongomes/" target="_blank" rel="noreferrer">Romilton Gomes</a></p>
+                <p className="text-light"><small><strong>SpringBoot React Week</strong><br />
+      Event promoted by DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
